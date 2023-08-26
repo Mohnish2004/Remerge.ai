fcd058fc7dcedb9626134bf7d90e098d7fd157d5
aaryanmohta04 <64299769+aaryanmohta04@users.noreply.github.com>
2023-08-26 16:44:54 -0700

    tried the rm -r -fo .git, lets see

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+
+import App from './App';
+
+const rootElement = 
+document.getElementById('root');
+const root = 
+createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
