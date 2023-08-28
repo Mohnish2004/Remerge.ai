8d55c15a54d2ae60b6675b7d626f3145dd0cdb69
Mohnish2004 <mohnishgopi@gmail.com>
2023-08-28 00:01:38 -0700

    New trusted companies changes

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
 return (
   <header class="bg-white">
 
-  <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+  <nav class="mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-8" aria-label="Global">
     <div class="flex lg:flex-1">
       <a href="#" class="-m-1.5 p-1.5">
         <span class="sr-only">Remerge</span>
@@ -24,14 +24,17 @@ return (
       </button>
     </div>
     <div class="hidden lg:flex lg:gap-x-12">
-
       <a href="#" class="text-sm font-regular leading-6 text-gray-900">Product</a>
       <a href="#" class="text-sm font-regular leading-6 text-gray-900">Features</a>
       <a href="#" class="text-sm font-regular leading-6 text-gray-900">Pricing</a>
       <a href="#" class="text-sm font-regular leading-6 text-gray-900">Company</a>
     </div>
-    <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+    <div class=" lg:flex lg:gap-x-12 inline-flex lg:flex-1 lg:justify-end items-center justify-center gap-x-6">
+    <a href="#" className="rounded-md bg-gray-600 px-3.5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" >
+    Login</a>
+    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+    Sign up
+    </a>
     </div>
   </nav>
 </header>
